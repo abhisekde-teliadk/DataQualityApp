357f18e2dbba9f9241a0671ccabc2381ceaf6a15
abhisekde-teliadk <abhisek.de@teliacompany.com>
2020-05-05 14:18:44 +0200

    try spark 2 scala code

--- src/main/scala/com/teliacompany/datamall/DataQualityApp.scala
+++ src/main/scala/com/teliacompany/datamall/DataQualityApp.scala
@@ -2,6 +2,10 @@ package com.teliacompany.datamall
 
 import org.apache.spark.sql.{SparkSession, SQLContext, DataFrame}
 
+import com.amazon.deequ.{VerificationSuite, VerificationResult}
+import com.amazon.deequ.VerificationResult.checkResultsAsDataFrame
+import com.amazon.deequ.checks.{Check, CheckLevel}
+
 object DataQualityApp {
     def main(args: Array[String]) = {
     val spark = SparkSession.builder.appName("DataQualityApp").getOrCreate()
@@ -17,8 +21,24 @@ object DataQualityApp {
                        .option("basePath", path)
                        .parquet(path + "/*")
 
+    val result: VerificationResult = { 
+        VerificationSuite().onData(work_amz_rev)
+                           .addCheck(
+                                Check(CheckLevel.Error, "Data Validation Check")
+                                    .hasCompleteness("customer_id", _ >= 0.90) // At least 90% rows have customer_id defined
+                                    .isUnique("review_id")
+                                    .isNonNegative("total_votes") 
+                                    .hasStandardDeviation("helpful_votes", _ < 3.0)
+                                    .hasEntropy("helpful_votes", _ < 2.0)
+                                    .hasCorrelation("helpful_votes", "total_votes", _ >= 0.8)
+                            )
+                            .run()
+    }
+    val output = checkResultsAsDataFrame(session, result)
+    output.show()
     println("+++ Results")
-    dataset.printSchema()
+    val out_name = root + "/" + pond + "/checks_" project + "_" + name
+    output.write.parquet(out_name)
     spark.stop()
 
     }
