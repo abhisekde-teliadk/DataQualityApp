f330df6bcc6184f96c15c2a21310182146a51b0b
abhisekde-teliadk <abhisek.de@teliacompany.com>
2020-05-05 08:28:55 +0200

    Adding dq.sh

---
+++ dq.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sbt package
+echo "Working on ${0}"
+#spark-submit --class "com.teliacompany.datamall.DataQualityApp" --master yarn --conf spark.ui.port=4444 --jars ./deequ/deequ-1.0.1.jar ./target/scala-2.10/dataquality_2.10-1.0.jar ${0}
