82c0ad0b648db4d03b75276bbc52722afa833cab
abhisekde-teliadk <abhisek.de@teliacompany.com>
2020-05-05 14:07:54 +0200

    try spark 2 scala code

--- src/main/scala/com/teliacompany/datamall/DataQualityApp.scala
+++ src/main/scala/com/teliacompany/datamall/DataQualityApp.scala
@@ -6,11 +6,19 @@ object DataQualityApp {
     def main(args: Array[String]) = {
     val spark = SparkSession.builder.appName("DataQualityApp").getOrCreate()
     import spark.implicits._
-    
-    val dataset = spark.read.parquet(args(0))
+
+    input = args(0)
+    project = input.split("/")(0)
+    name = input.split("/")(1)
+    pond = name.split("_")(0)
+    root = "/data"
+    path = root + "/" + pond + "/" + project + "/" + name + "/" + "data"
+    val dataset = spark.read
+                       .option("basePath", path)
+                       .parquet(path + "/*")
 
     println("+++ Results")
-    dataset.show()
+    dataset.printSchema()
     spark.stop()
 
     }
