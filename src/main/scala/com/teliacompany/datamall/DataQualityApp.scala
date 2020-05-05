0978d9920dac5e2ef4fa64dbdee39f1f615954c1
abhisekde-teliadk <abhisek.de@teliacompany.com>
2020-05-05 12:57:54 +0200

    try spark 2 scala code

--- src/main/scala/com/teliacompany/datamall/DataQualityApp.scala
+++ src/main/scala/com/teliacompany/datamall/DataQualityApp.scala
@@ -1,12 +1,12 @@
 package com.teliacompany.datamall
 
 import org.apache.spark.sql.{SparkSession, SQLContext, DataFrame}
-import spark.implicits._
 
 object DataQualityApp {
     def main(args: Array[String]) = {
     val spark = SparkSession.builder.appName("DataQualityApp").getOrCreate()
-        
+    import spark.implicits._
+    
     val dataset = spark.read.parquet(args(0))
 
     println("+++ Results")
