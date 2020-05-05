2956115b2637dc4b01c2bb38cf087c5867f92515
abhisekde-teliadk <abhisek.de@teliacompany.com>
2020-05-05 08:39:54 +0200

    Bugfix

--- src/main/scala/com/teliacompany/datamall/DataQualityApp.scala
+++ src/main/scala/com/teliacompany/datamall/DataQualityApp.scala
@@ -11,14 +11,14 @@ object DataQualityApp {
     def main(args: Array[String]) = {
         val conf = new SparkConf().setAppName("Hoad HDFS").setMaster("yarn-client")
         val sc = new SparkContext(conf)
-        load(args(0))
+        load(args(0), sc)
         sc.stop()
     }
 
     def load(path: String) = {
         val ls = HdfsUtils.getAllFiles(path)
         println("+++ Files list: ")
-        printn(ls)
+        println(ls)
     }
 }
 
