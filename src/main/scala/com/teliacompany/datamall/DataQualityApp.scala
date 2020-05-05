8ffdb36ded689898b3406fef0ee45de325714114
abhisekde-teliadk <abhisek.de@teliacompany.com>
2020-05-05 15:00:18 +0200

    try constraints

--- src/main/scala/com/teliacompany/datamall/DataQualityApp.scala
+++ src/main/scala/com/teliacompany/datamall/DataQualityApp.scala
@@ -2,9 +2,7 @@ package com.teliacompany.datamall
 
 import org.apache.spark.sql.{SparkSession, SQLContext, DataFrame}
 
-import com.amazon.deequ.{VerificationSuite, VerificationResult}
-import com.amazon.deequ.VerificationResult._
-import com.amazon.deequ.checks.{Check, CheckLevel}
+import com.amazon.deequ.suggestions.{ConstraintSuggestionRunner, Rules}
 
 object DataQualityApp {
     def main(args: Array[String]) = {
@@ -20,26 +18,24 @@ object DataQualityApp {
     val dataset = spark.read
                        .option("basePath", path)
                        .parquet(path + "/*")
-    /*
-    val result: VerificationResult = { 
-        VerificationSuite().onData(dataset)
-                           .addCheck(
-                                Check(CheckLevel.Error, "Data Validation Check")
-                                    .hasCompleteness("customer_id", _ >= 0.90) // At least 90% rows have customer_id defined
-                                    .isUnique("review_id")
-                                    .isNonNegative("total_votes") 
-                                    .hasStandardDeviation("helpful_votes", _ < 3.0)
-                                    .hasEntropy("helpful_votes", _ < 2.0)
-                                    .hasCorrelation("helpful_votes", "total_votes", _ >= 0.8)
-                            )
-                            .run()
+
+    val result = { 
+    ConstraintSuggestionRunner()
+        .onData(dataset)
+        .addConstraintRules(Rules.DEFAULT) 
+        .run()
     }
-    val output = checkResultsAsDataFrame(spark, result)
+
+    val output = result.constraintSuggestions
+                       .flatMap { 
+                                    case (column, suggestions) =>  suggestions.map { 
+                                        constraint => (column, constraint.description, constraint.codeForConstraint)  
+                                    } 
+                        }.toSeq.toDF("column", "suggestion", "code_snippet")
     output.show()
     println("+++ Results")
     val out_name = root + "/" + pond + "/checks_" + project + "_" + name    
     output.write.parquet(out_name)
-    */
     spark.stop()
 
     }
