91ba15d2e6ad44d86f5c8f3123b658baafa0d26e
abhisekde-teliadk <abhisek.de@teliacompany.com>
2020-05-05 12:51:26 +0200

    try spark 2 scala code

--- src/main/scala/com/teliacompany/datamall/DataQualityApp.scala
+++ src/main/scala/com/teliacompany/datamall/DataQualityApp.scala
@@ -4,18 +4,25 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.SQLContext
 import com.teliacompany.datamall._
 
+import com.amazon.deequ.{VerificationSuite, VerificationResult}
+import com.amazon.deequ.VerificationResult.checkResultsAsDataFrame
+import com.amazon.deequ.checks.{Check, CheckLevel}
+
 object DataQualityApp {
     def main(args: Array[String]) = {
-        val spark = SparkSession.builder.appName("DataQualityApp").getOrCreate()
-        // DataFrame and toDF support
-        import org.apache.spark.sql.DataFrame
-        val session = sqlContext.sparkSession
-        import session.sqlContext.implicits._
-        println("+++ Working on " + args(0))
-        val dataset = sqlContext.read.parquet(args(0))
-        println("+++ Results")
-        dataset.show()
-        sc.stop()
+    val spark = SparkSession.builder.appName("DataQualityApp").getOrCreate()
+
+    // DataFrame and toDF support
+    import org.apache.spark.sql.DataFrame
+    val session = sqlContext.sparkSession
+    import session.sqlContext.implicits._
+        
+    val dataset = sqlContext.read.parquet(args(0))
+
+    println("+++ Results")
+    dataset.show()
+    sc.stop()
+    
     }
 }
 
