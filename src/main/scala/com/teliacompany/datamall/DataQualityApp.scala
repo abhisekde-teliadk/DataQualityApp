61ccaf9f0ab14f70f940b0a2b42b3e43d201ca3f
abhisekde-teliadk <abhisek.de@teliacompany.com>
2020-05-07 18:49:02 +0200

    compute anomaly

--- src/main/scala/com/teliacompany/datamall/DataQualityApp.scala
+++ src/main/scala/com/teliacompany/datamall/DataQualityApp.scala
@@ -22,8 +22,10 @@ object DataQualityApp {
     val spark = SparkSession.builder.appName("DataQualityApp").getOrCreate()
     import spark.implicits._
 
-    if (args.length == 0)
-        throw new Exception("No dataset provided. Please pass path of a dataset as argument.")
+    // if (args.length == 0)
+    //     throw new Exception("No dataset provided. Please pass path of a dataset as argument.")
+    require(args.length == 2 && args(1).startsWith("/") && (args(0) == "--execute" || args(0) == "--check"), 
+            "Usage: Requires 2 argument: \n - Execution mode: [--execute | --check]\n - <full path to parquet dataset> \nspark2-submit --class \"com.teliacompany.datamall.DataQualityApp\" \\ \n --master yarn \\ \n --conf spark.ui.port=XXXX \\ \n /path/to//dataquality_xxxx.jar <execution_mode> <full path to parquet dataset>\n")
 
     val path        = args(0)
     val p_items     = path.split("/")
@@ -53,18 +55,19 @@ object DataQualityApp {
                   .parquet(out_metric + "/*")
     val rows = metrics.count
     println("Metrices record count: " + rows.toString)
+    if(args(0) == "--check") {
+        println("Thresholds for anomaly")
+        val stage3 = calc_thresholds(in_name, df, metrics, spark) // Calculate boundaries of acceptable values
+        stage3.show(100)
 
-    println("Thresholds for anomaly")
-    val stage3 = calc_thresholds(in_name, df, metrics, spark) // Calculate boundaries of acceptable values
-    stage3.show(100)
-
-    println("+++ Anomaly Check Results: " + out_checks)
-    val stage4 = apply_checks(in_name, df, stage3, spark) // Anomaly detection by comparing with historical metrices
-    stage4.write
-          .mode(SaveMode.Overwrite)
-          .parquet(out_checks)
-    stage4.show(100)  
-    stage4.printSchema
+        println("+++ Anomaly Check Results: " + out_checks)
+        val stage4 = apply_checks(in_name, df, stage3, spark) // Anomaly detection by comparing with historical metrices
+        stage4.write
+            .mode(SaveMode.Overwrite)
+            .parquet(out_checks)
+        stage4.show(100)  
+        // stage4.printSchema
+        }
     spark.stop()
 
     }
